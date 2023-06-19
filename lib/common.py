b198d0e224be5eb8276c9952548c8cd7d7bcab6e
jcarlos5 <josecarlosvilchezvillegas@gmail.com>
2023-06-18 21:24:28 -0500

    fix: Add camera capture

--- lib/common.py
+++ lib/common.py
@@ -1,4 +1,5 @@
 import json
+import cv2
 
 class SETTINGS:
     CONFIDENCE_TRESHOLD:float = 0.5
@@ -15,3 +16,43 @@ class SETTINGS:
         with open(fname, 'r') as fp:
             for k, v in json.load(fp).items():
                 setattr(SETTINGS, k, v)
+
+
+def open_cam_usb():
+    """
+    Initialize video stream capture
+
+    Returns:
+    VideoCapture
+    """
+    vs = cv2.VideoCapture(0)
+    vs.set(cv2.CAP_PROP_FRAME_WIDTH, SETTINGS.CAM_WITH)
+    vs.set(cv2.CAP_PROP_FRAME_HEIGHT, SETTINGS.CAM_HEIGHT)
+
+    return vs
+
+
+def letterbox(im, new_shape:tuple=tuple(SETTINGS.IMAGE_SIZE), color:tuple=(0, 0, 0)):
+    """
+    Resize and pad image while meeting stride-multiple constraints
+    """
+
+    shape = im.shape[:2]
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+
+    return im, ratio, (dw, dh)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # from rknnlite.api import RKNNLite
-from lib.common import SETTINGS
+from lib.common import SETTINGS, open_cam_usb, letterbox
+from imutils.video import FPS
 from lib.process import nms
 import argparse
+import time
 import cv2
 
 ap = argparse.ArgumentParser()
@@ -32,8 +34,6 @@ def draw(image, bboxes, scores, classes):
         
 
 if __name__ == '__main__':
-    pass
-
     # rknn_lite = RKNNLite()
     # ret = rknn_lite.load_rknn(SETTINGS.MODEL_PATH)
 
@@ -41,4 +41,30 @@ if __name__ == '__main__':
     
     # ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0)
 
-    # vs = open_cam_usb(config.CAM_DEV, config.CAM_WIDTH, config.CAM_HEIGHT)
+    vs = open_cam_usb()
+    time.sleep(2.0)
+    fps = FPS().start()
+
+    if not vs.isOpened():
+        print("Cannot capture from camera. Exiting.")
+        quit()
+    
+    prev_frame_time = 0
+    new_frame_time = 0
+
+    while True:
+        ret, frame = vs.read()
+        if not ret: break
+
+        new_frame_time = time.time()
+        show_fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
+        show_fps = int(show_fps)
+        show_fps = str("{} FPS".format(show_fps))
+
+        ori_frame = frame
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame, ratio, (dw, dh) = letterbox(frame)
+
+        
